228329fe72a9127c7e4c157e32bb80cdd1826e5f
checker5965 <archit.checker_ug20@ashoka.edu.in>
2018-12-02 22:04:16 +0530

    cleaned iris

--- floatify.py
+++ floatify.py
@@ -8,11 +8,11 @@ for row in reader:
     outrow = ''
     for col in row:
         if col == "Iris-virginica":
-            outrow = outrow+'3\n'
-        elif col == "Iris-versicolor":
             outrow = outrow+'2\n'
-        elif col == "Iris-setosa":
+        elif col == "Iris-versicolor":
             outrow = outrow+'1\n'
+        elif col == "Iris-setosa":
+            outrow = outrow+'0\n'
         else:
             outrow = outrow + col + ','
     fout.write(outrow)    
